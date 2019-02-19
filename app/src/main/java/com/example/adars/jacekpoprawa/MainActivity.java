3cba2826b9c074912e7040517b6b33b9c2e0e91b
Bacho172 <adarsim1@op.pl>
2019-02-19 22:32:46 +0100

    Spinners added

--- app/src/main/java/com/example/adars/jacekpoprawa/MainActivity.java
+++ app/src/main/java/com/example/adars/jacekpoprawa/MainActivity.java
@@ -4,6 +4,10 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.adars.jacekpoprawa.Core.Settings;
@@ -15,22 +19,52 @@ import com.example.adars.jacekpoprawa.DomainModel.Country;
 import com.example.adars.jacekpoprawa.DomainModel.Product;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinnerCities;
+    private Spinner spinnerCountries;
+    private EditText editTextProductName;
+    private Button buttonAdd;
+
+    private ArrayList<City> _cities;
+    private ArrayList<Country> _countries;
+    private CityDAO _cityDAO;
+    private CountryDAO _countryDAO;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SQLiteDatabase base = openOrCreateDatabase("baza.db", Context.MODE_PRIVATE,  null);
+        _cityDAO = new CityDAO(this);
+        _countryDAO = new CountryDAO(this);
+        _countries = _countryDAO.getAll();
+        _cities = _cityDAO.getAll();
 
-        /* Tworzenie tabel jeśli te nie istnieją */
-        base.execSQL(Settings.tableCountry);
-        base.execSQL(Settings.tableCity);
-        base.execSQL(Settings.tableProduct);
+        ArrayList<String> citiesStr = new ArrayList<>();
+        ArrayList<String> countiriesStr = new ArrayList<>();
+        for (City c : _cities) citiesStr.add(c.getName());
+        for (Country c : _countries) countiriesStr.add(c.getName());
 
-        base.close();
+        // do zaimplementowania
+        spinnerCities = findViewById(R.id.spinner_cities);
+        spinnerCountries = findViewById(R.id.spinner_countries);
+//        editTextProductName = findViewById(R.id.edit_text_product_name);
+//        buttonAdd = findViewById(R.id.button_add);
+        setDataToSpinner(spinnerCities, citiesStr);
+        setDataToSpinner(spinnerCountries, countiriesStr);
+
+
+//        SQLiteDatabase base = openOrCreateDatabase("baza.db", Context.MODE_PRIVATE,  null);
+//
+//        /* Tworzenie tabel jeśli te nie istnieją */
+//        base.execSQL(Settings.tableCountry);
+//        base.execSQL(Settings.tableCity);
+//        base.execSQL(Settings.tableProduct);
+//
+//        base.close();
 
         /*Testowanie danych (można później wykomentować*/
         CityDAO cityDAO = new CityDAO(this);
@@ -60,4 +94,12 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
 
     }
+
+    void setDataToSpinner(Spinner spinner, List<String> data) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, data);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
 }
